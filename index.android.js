618ab69b1d386da43bd3e789085c4524eb22b720
Aaron Stanley <mail@aaron-stanley.me>
2016-11-03 16:01:09 +0800

    first commit

---
+++ index.android.js
@@ -0,0 +1,143 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+ 'use strict';
+import autobind from 'autobind-decorator'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Dimensions,
+  GeoLocation
+} from 'react-native';
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
+var location;
+
+
+export default class InvoDay extends Component {
+
+state = {
+    lat: 'unknown',
+    lng: 'unknown',
+    city: 'unknown',
+    show : false
+  };
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello, Welcome to my demo for react native on Android,
+        </Text>
+    
+        <TouchableNativeFeedback
+        	onPress={this._click.bind(this)}
+        	background={TouchableNativeFeedback.SelectableBackground()}>
+      		<View style={styles.button}>
+        		<Text style={styles.button_text}>So, where are you? click here to find out.</Text>
+      		</View>
+    	</TouchableNativeFeedback>
+    	
+    	{this._renderLocation()}   
+    	
+    </View>
+    );
+    }
+    
+    
+    _renderLocation() {
+        if (this.state.show) {
+            return (
+            <View style={styles.container1}>
+        		<Text style={styles.instructions}>
+          			<Text style={styles.title}>Ah, your in {this.state.city}, What would you like to know?</Text>
+        		</Text>
+        		<TouchableNativeFeedback
+        			onPress={this._click.bind(this)}
+        			background={TouchableNativeFeedback.SelectableBackground()}>
+      				<View style={styles.button}>
+        				<Text style={styles.button_text}>Five Day Weather</Text>
+      				</View>
+    			</TouchableNativeFeedback>
+    			</View>
+            );
+        } else {
+            return null;
+        }
+    }
+    
+    _click() {
+		console.log("Clicked");
+		navigator.geolocation.getCurrentPosition(
+      		(position) => {
+        		var lat = position.coords.latitude;
+        		var lng = position.coords.longitude;
+        		console.log(position);
+        		this.setState({lat,lng});
+        		
+        		var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' +lat+','+lng+'&language=en&key=AIzaSyBDEkbnMa5p8AEKFlL4XXMIFo5JAqzsG-Q'
+        		
+        		fetch(url)
+        		.then((response) => response.json())
+        		.then((responseJson) => {
+        			var address_components = responseJson.results[0].address_components;
+        			for (var i = 0; i < address_components.length; i++) {
+        				if(address_components[i].types[0] == 'locality') {
+        					var city = address_components[i].long_name;
+        					var show = true;
+        					this.setState({city,show});
+						}
+					}
+      			});
+        		
+      		},
+      		(error) => alert(JSON.stringify(error)),
+    	);
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  
+  container1: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  button: {
+  	width: width * 0.8,
+  	height: 40, 
+  	backgroundColor: 'red',
+    marginTop: 5,
+    marginBottom: 5
+  },
+  button_text: {
+  	textAlign: 'center',
+  	marginTop: 10
+  }
+});
+
+AppRegistry.registerComponent('InvoDay', () => InvoDay);
